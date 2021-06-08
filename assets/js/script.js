cb80b34f590817f7cc96870ad217c735b12af574
Bernadette McKnight <sissyhanks@yahoo.com>
2021-06-08 06:35:37 -0400

    add images

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,78 +11,33 @@ var newUse;
 var list = document.getElementById("history");
 var buttArr = localStorage.getItem('wishCities');
 
+var hotCity = document.querySelector("#hotCity");
+var wind = document.querySelector("#wind");
+var hu = document.querySelector("#hu");
+var uvi = document.querySelector("#uv");
+
+
+
 // if there are cities in memory from previous visit will call that data
 var cityWish; 
 if (buttArr == null) {
   cityWish = [];
 } else if (buttArr !== null) {
   cityWish = JSON.parse(buttArr);
+  makeMemory();
 }
 
 // function that will make buttons of previous cities visited if stored in memory
 function makeMemory () {
+    $( ".saved-city" ).remove();
   for(var i = 0; i < JSON.parse(buttArr).length; i++){
     butt = document.createElement('button');
     butt.setAttribute("id", JSON.parse(buttArr)[i]);
     butt.setAttribute("class", "saved-city btn w-100 text-center mt-2");
     butt.innerText = JSON.parse(buttArr)[i];
-    list = document.getElementById("history");
 
     $( "#history" ).append( butt );
   }
-}
-
-function makeHistory (){
-  butt = document.createElement('button');
-  butt.setAttribute("id", historyName);
-  butt.setAttribute("class", "saved-city btn w-100 text-center mt-2");
-  butt.innerText = historyName;
-  list = document.getElementById("history");
-
-  $( "#history" ).prepend( butt );
-}
-
-
-// adds button of searched city to list of city search buttons
-function makeHay () {
-
-  //   butt = document.createElement('button');
-  // butt.setAttribute("id", historyName);
-  // butt.setAttribute("class", "saved-city btn w-100 text-center mt-2");
-  // butt.innerText = historyName;
-  // list = document.getElementById("history");
-
-    $( hSearch ).each(function( index ) {
-    console.log( index + ": " + $( this ).text() );
-    if ($( this ).text() == citySearch){
-    $( this ).remove();
-    }
-  });
-
-  makeHistory();
-  
-  // butt = document.createElement('button');
-  // butt.setAttribute("id", historyName);
-  // butt.setAttribute("class", "saved-city btn w-100 text-center mt-2");
-  // butt.innerText = historyName;
-  // list = document.getElementById("history");
-
-
-  
-  // $( "#history" ).prepend( butt );
-
-  $( hSearch ).click(function(){
-    citySearch = this.id;
-    citySpot();
-  });
-  
-  cityWish = [];
-  
-  $( hSearch ).each(function() {
-    cityWish.push($( this ).text());
-    localStorage.setItem('wishCities', JSON.stringify(cityWish));
-  });
-
 }
 
 function citySpot(){
@@ -105,45 +60,94 @@ function citySpot(){
     trop = (data.coord.lat);
     mer = (data.coord.lon);
     historyName = (data.name);
-    console.log(trop, mer);
-    // cityWish.unshift(historyName);
-    // localStorage.setItem('wishCities', JSON.stringify(cityWish));
-    
-    makeHay();
 
-    oneCall = 'https://api.openweathermap.org/data/2.5/onecall?lat=' +JSON.stringify(trop)+ '&lon=' +JSON.stringify(mer)+ '&appid=91db1e3bfaa9e864b329e2d641b22377&units=imperial';
+      oneCall = 'https://api.openweathermap.org/data/2.5/onecall?lat=' +JSON.stringify(trop)+ '&lon=' +JSON.stringify(mer)+ '&appid=91db1e3bfaa9e864b329e2d641b22377&units=imperial';
     
-    fetch(oneCall)
+      fetch(oneCall)
     
-    .then(function (response) {
+      .then(function (response) {
       return response.json();
       })
+
       .then(function (data) {
       //Using console.log to examine the data
       console.log(data);
-    });
+      hotCity.innerText = ("hi");
+      wind.innerText = (data.current.wind_speed);
+      hu.innerText = (data.current.humidity);
+      uvi.innerText = (data.current.uvi);
+      })
 
-    });
+  newInTown ();
+        
+  })
+}
+
+function startHistory () {
+    butt = document.createElement('button');
+    butt.setAttribute("id", cityWish);
+    butt.setAttribute("class", "saved-city btn w-100 text-center mt-2");
+    butt.innerText = cityWish;
+
+    $( "#history" ).append( butt );
+}
+
+function makeHistory (){
+  $( ".saved-city" ).remove();
+  for(var i = 0; i < cityWish.length; i++){
+    butt = document.createElement('button');
+    butt.setAttribute("id", cityWish[i]);
+    butt.setAttribute("class", "saved-city btn w-100 text-center mt-2");
+    butt.innerText = cityWish[i];
+
+    $( "#history" ).append( butt );
+  }
+}
+
+function newInTown () {
+  console.log(historyName);
+
+  if (cityWish.indexOf(historyName) === -1){
+      cityWish.push(historyName)
+            localStorage.setItem("wishCities", JSON.stringify(cityWish));
+            startHistory ();
+  }
+  else {
+  let temparr = []
+  for(let  i=0; i<cityWish.length; i++){
+    
+    if (cityWish[i] !== historyName){
+      temparr.push(cityWish[i]);
+      // makeHistory ();
+    } else {
+      temparr.push(historyName);
+      console.log(historyName);
+      // makeHistory ();
+    }
+    // makeHistory ();
+  }     
+  cityWish = temparr;
+      localStorage.setItem("wishCities", JSON.stringify(cityWish));
+  }
 }
 
 function cityReset(){
   document.getElementById( "lookUp" ).value='';
 }
 
-// start active script
-if (buttArr !== null){
-  makeMemory();
-}
-
-console.log(hSearch.length);
-
 bSearch.addEventListener("click", function(){
   citySearch = lookUp.value;
-  citySpot();
+  console.log(cityWish);
+  console.log(citySearch);
+  citySpot ();
   cityReset();
 });
 
 $( hSearch ).click(function(){
-  citySearch = this.id;
-  citySpot();
+  citySearch = $(this).attr("id");
+  console.log(cityWish);
+  console.log(citySearch);
+  citySpot ();
+  cityReset();
 });
+
